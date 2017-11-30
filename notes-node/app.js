f44e3052eb99036d4f34a48fbe5630cf77272e0b
Samuel Rafini <Sammy@MacBook-Air.local>
2017-12-01 00:38:53 +0100

    Using modules.exports

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,15 +1,21 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res);
 
-//creates a new file if it does not exist and appends 'hello world!'
-//fs.appendFile('greeting.txt', 'Hello ' + user.username + "!");
+var sum = notes.addNumbers(9,-2);
+console.log(sum);
 
-//better way to use the string
-fs.appendFile('greeting.txt', `Hello ${user.username}!`);
+// var user = os.userInfo();
 
+// //creates a new file if it does not exist and appends 'hello world!'
+// //fs.appendFile('greeting.txt', 'Hello ' + user.username + "!");
+
+// //better way to use the string
+// fs.appendFile('greeting.txt', `Hello ${user.username}! you are ${notes.age}`);
 
 
