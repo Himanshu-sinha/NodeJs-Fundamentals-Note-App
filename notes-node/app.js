2ccf312a5527ac9cbde648f0e70ecb8a186af78a
Samuel Rafini <Sammy@MacBook-Air.local>
2017-12-02 20:45:52 +0100

    using 3rd party module like npm

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,9 +3,16 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ =  require('lodash');
+
+console.log(_.isString(true));
+console.log(_.isString('Samuel'));
 
 var res = notes.addNote();
 console.log(res);
+var filteredArray = _.uniq(['Samuel', 1, 'Samuel', 1, 2, 3, 3, 4, 5, 6, 7]);
+
+console.log(filteredArray);
 
 var sum = notes.addNumbers(9,-2);
 console.log(sum);
