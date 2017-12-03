8fed4817d178db10957e48ad2730b41b97df52ff
Samuel Rafini <Sammy@MacBook-Air.local>
2017-12-03 11:44:35 +0100

    Getting input from user

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,28 +1,21 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ =  require('lodash');
 
-console.log(_.isString(true));
-console.log(_.isString('Samuel'));
+const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
-var filteredArray = _.uniq(['Samuel', 1, 'Samuel', 1, 2, 3, 3, 4, 5, 6, 7]);
+console.log(process.argv);
+var command = process.argv[2];
+console.log('Command', command);
 
-console.log(filteredArray);
+if(command === 'add'){
+    console.log('Adding new note');
+    
+}else if(command === 'list'){
+    console.log('Listing all notes');
 
-var sum = notes.addNumbers(9,-2);
-console.log(sum);
-
-// var user = os.userInfo();
-
-// //creates a new file if it does not exist and appends 'hello world!'
-// //fs.appendFile('greeting.txt', 'Hello ' + user.username + "!");
-
-// //better way to use the string
-// fs.appendFile('greeting.txt', `Hello ${user.username}! you are ${notes.age}`);
-
+}else{
+    console.log('Command not recognized');
+}
 
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,16 +1,9 @@
 console.log('Starting notes.js');
 
-//console.log(module);
 
-//module.exports.age = 25;
 
 module.exports.addNote = () => {
 console.log('addNote');
 return 'New note';
 };
 
-module.exports.addNumbers = function(x, b){
-    var sum = x + b;
-    return sum;
-
-};
